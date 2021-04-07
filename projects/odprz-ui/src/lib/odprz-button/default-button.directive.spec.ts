7f10cf3a09b1f62bacff59eb40fd337a99e873be
Oscar Daniel Perez <odprz.dev@gmail.com>
2021-04-06 20:25:20 -0500

    feat: add default button style

---
+++ projects/odprz-ui/src/lib/odprz-button/default-button.directive.spec.ts
@@ -0,0 +1,8 @@
+import { DefaultButtonDirective } from './default-button.directive';
+
+describe('DefaultButtonDirective', () => {
+  it('should create an instance', () => {
+    const directive = new DefaultButtonDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ projects/odprz-ui/src/lib/odprz-button/default-button.directive.ts
@@ -0,0 +1,49 @@
+import { Directive, ElementRef, Input } from '@angular/core';
+
+@Directive({
+  selector: '[odprz-btn]'
+})
+export class DefaultButtonDirective {
+
+  @Input() color:string;
+  @Input() ovalVariant:boolean = false;
+
+  constructor(private el:ElementRef) {
+    let tagName = el.nativeElement.tagName;
+
+    if(tagName === 'BUTTON' || tagName ==="A"){
+      el.nativeElement.className="odprz-btn-default";
+    }
+
+  }
+
+  ngOnInit(): void {
+    console.log('input: ',this.color)
+    if(this.color){
+      switch (this.color) {
+        case 'warn':
+          console.log('warn')
+          this.el.nativeElement.classList.add('odprz-btn-warn');
+          break;
+          case 'alert':
+          this.el.nativeElement.classList.add('odprz-btn-alert');
+          break;
+          case 'primary':
+          this.el.nativeElement.classList.add('odprz-btn-primary');
+          break;
+          case 'secundary':
+          this.el.nativeElement.classList.add('odprz-btn-secundary');
+          break;
+        default:
+          break;
+      }
+    }
+
+    if(this.ovalVariant){
+      console.log(this.ovalVariant);
+      this.el.nativeElement.classList.add('btn-oval')
+    }
+
+  }
+
+}
